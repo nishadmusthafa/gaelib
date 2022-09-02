5c2bc23104f9c7946e930c239f64fb6a1a1d2fbe
Nishad <nishadmusthafa@gmail.com>
2022-09-02 14:52:54 +0100

    Adding template data to package

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ import glob
 
 setup(
     name='gaelib',
-    version='0.1.0',
+    version='1.0.0',
     description='Google App Engine Library for Flask',
     author='Nishad Musthafa',
     license='gpl-3.0',
     packages=find_packages(where='.') + find_packages(exclude=("tests",)),
+    package_data={'': ['dashboard/templates/*.html']},
     include_package_data=True,
     zip_safe=False,
     install_requires=[
